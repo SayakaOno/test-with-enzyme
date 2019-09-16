b0e5becded745a1e32d7a4739d5c3c65e6e88f3f
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-09-15 21:43:19 -0700

    add Headline component with tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/header';
+import Headline from './components/headline';
 import './app.scss';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Headline header="post" desc="Click the button to render props" />
     </div>
   );
 };
