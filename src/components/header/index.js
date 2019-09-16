b0a72860ebbd88d8bec27a5a4ae3d6570f461262
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-09-15 19:10:00 -0700

    initial commit of my component without tests

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="wrapper">Title</div>
+    </header>
+  );
+};
+
+export default Header;
